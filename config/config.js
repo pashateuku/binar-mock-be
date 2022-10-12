908a0d82c270f9d69c9df132221b1d687614dd45
pashateuku <thariqteuku.soc@gmail.com>
2022-10-12 21:00:47 +0700

    make sequelize config dynamic with dotenv

--- config/config.js
+++ config/config.js
@@ -2,24 +2,42 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": "postgres",
-    "password": "sakasirasaka21",
-    "database": "binar-mock",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+    "username": process.env.DATABASE_USERNAME,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME__DEV,
+    "host": process.env.DATABASE_HOST,
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DATABASE_USERNAME,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME__TEST,
+    "host": process.env.DATABASE_HOST,
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DATABASE_USERNAME,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME__PROD,
+    "host": process.env.DATABASE_HOST,
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
   }
 }
